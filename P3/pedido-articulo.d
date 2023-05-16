1e53b6e6eac0fb8e9756b8c8aa702f54c8a36db5
Antonio Richarte González <101519278+antoniorichartedev@users.noreply.github.com>
2023-05-16 19:25:04 +0200

    Archivos basura eliminados

--- P3/pedido-articulo.d
+++
@@ -1,3 +0,0 @@
-pedido-articulo.o pedido-articulo.d: pedido-articulo.cpp pedido-articulo.hpp articulo.hpp \
-  ../P1/fecha.hpp ../P1/cadena.hpp pedido.hpp tarjeta.hpp usuario.hpp \
-  usuario-pedido.hpp
